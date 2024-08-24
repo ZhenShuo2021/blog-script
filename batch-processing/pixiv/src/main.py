cc8c99d7b810a66c5a22465048b29ba7765fbaa1
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2024-08-24 12:39:29 +0800

    Use all modules in main.py

--- batch-processing/pixiv/src/main.py
+++ batch-processing/pixiv/src/main.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import src
 import src.categorizer
 import src.retriever
 import src.synchronizer
@@ -25,7 +24,7 @@ def main():
     # Or categorize specific category
     # categories = list(config_loader.get_categories())
     # file_categorizer.categorize(categories[1])   # categorize the last category
-    file_count = 0
+    file_count = file_utils.count_files(combined_paths, "local_path")
 
     logger.info("開始同步檔案...")
     log_dir = root / Path("data")
@@ -35,12 +34,11 @@ def main():
     # file_syncer.sync_folders(combined_paths["IdolMaster"]["local_path"], combined_paths["IdolMaster"]["remote_path"])
     # src.synchronizer.LogMerger(log_dir).merge_logs()
     
-    # logger.info("開始尋找遺失作品...")
-    # src.retriever.retrieve_artwork()
+    logger.info("開始尋找遺失作品...")
+    src.retriever.retrieve_artwork()
 
-    # src.viewer.count_tags(combined_paths.get("remote_path"), output_file=src.viewer.file_name)
-    # tag_counts = src.viewer.read_tag_counts(src.viewer.file_name)
-    # src.viewer.plot_pie_chart(tag_counts, top_n=15, skip=2, output_file=src.viewer.file_name) # skip since the top tags are useless
+    logger.info("開始統計標籤...")
+    src.viewer.viewer_main(config_loader)
 
     print(f"\033[32m這次新增了\033[0m\033[32;1;4m {file_count} \033[0m\033[32m個檔案🍺\033[0m")
 
